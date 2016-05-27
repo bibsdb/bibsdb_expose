b8d51834b3f881910e2905121eaf8a9fa8787b90
Agnete Moos <agms@sonderborg.dk>
2016-05-27 13:45:08 +0200

    Add walls to ding_page and ding_group

--- scripts/bibsdb_expose.js
+++ scripts/bibsdb_expose.js
@@ -4,16 +4,22 @@
  /**
   * Remove panel pane padding on empty walls
   */
-  function remove_padding_from_empty_wall() {
-    //Attach menu classes
-    $(".pane-bibsdb-wall-panel-bibsdb-wall").has(".view-bibsdb-wall.empty").addClass("nopadding");
+  function empty_wall() {
+    //Attach class if wall is empty
+    $(".pane-bibsdb-wall-panel-bibsdb-wall").has(".view-bibsdb-wall.empty").addClass("nocontent");
+
+    // Attach class if an editor is logged in
+    $(".default-layout").has(".contextual-links-region .view-bibsdb-wall").addClass("editormode");
+    $(".pane-bibsdb-wall-panel-bibsdb-wall").has(".contextual-links-region .view-bibsdb-wall").addClass("editormode");
+
+    // Attach class to default layout if wall0 (rigth column wall) is empty
+    $(".default-layout").has(".wall0.nocontent").addClass("empty-wall0");
   }
 
 
   // When ready start the magic.
   $(document).ready(function () {
-    console.log("hej");
-    remove_padding_from_empty_wall();
+    empty_wall();
   });
 
 })(jQuery);
