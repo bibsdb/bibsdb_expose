dcfd6e44bfe8f839e7e970c7e9a8e15b4980835f
Agnete Moos <agms@sonderborg.dk>
2016-07-12 16:17:17 +0200

    Different bugfixes

--- scripts/bibsdb_expose.js
+++ scripts/bibsdb_expose.js
@@ -16,11 +16,25 @@
     $(".default-layout").has(".wall0.nocontent").addClass("empty-wall0");
   }
 
+  /**
+  * Detect secondary content and append class, to have walls widths differ
+  */
+  function add_secondary_content_class() {
+
+    $(".node-type-ding-page .default-layout").has(".secondary-content").find(".wall").addClass("seccontent").find("view-bibsdb-wall");
+    $(".node-type-ding-group .default-layout").has(".secondary-content").find(".wall").addClass("seccontent");   
+
+    $(".node-type-ding-page .default-layout").has(".secondary-content").find(".wall.right .view-bibsdb-wall").removeClass("col2 col3").addClass("col1");
+    $(".node-type-ding-group .default-layout").has(".secondary-content").find(".wall.right .view-bibsdb-wall").removeClass("col2 col3").addClass("col1");
+  
+  }
+
   
 
   // When ready start the magic.
   $(document).ready(function () {
     empty_wall();
+    add_secondary_content_class();
   });
 
 })(jQuery);
