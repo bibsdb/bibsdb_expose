534566224b700ef078efbce293f95950e0c6c3df
Agnete Moos <agms@sonderborg.dk>
2016-12-19 09:16:42 +0100

    Move code from this module to theme

--- scripts/bibsdb_expose.js
+++ scripts/bibsdb_expose.js
@@ -9,7 +9,6 @@
     $(".pane-bibsdb-wall-panel-bibsdb-wall").has(".view-bibsdb-wall.empty").addClass("nocontent");
 
     // Attach class if an editor is logged in
-    $(".default-layout").has(".contextual-links-region .view-bibsdb-wall").addClass("editormode");
     $(".pane-bibsdb-wall-panel-bibsdb-wall").has(".contextual-links-region .view-bibsdb-wall").addClass("editormode");
 
     // Attach class to default layout if wall0 (rigth column wall) is empty
